902733b1a11215bd8e614b6171bbf0f020c63788
Alex Bruce <brucealmightyalex@yahoo.com>
2018-07-17 15:37:04 -0600

    add CurrentWeather component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import CurrentWeather from './CurrentWeather';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.location = {
+      
+    }
+  }
+
+  addLocation = () => {
+    
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Weathrly</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, add your location here:
+          <input type="text" />
+          <button onClick={this.addLocation}>Submit</button>
+          <CurrentWeather />
         </p>
       </div>
     );
