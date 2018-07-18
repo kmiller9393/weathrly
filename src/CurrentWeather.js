b81539809cb500fb89e091e9ef704f4510802cf2
Kurt Miller <kmiller9393@gmail.com>
2018-07-17 21:19:45 -0600

    Add basic weather information that will be within the CurrentWeather component

--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -11,7 +11,15 @@ class CurrentWeather extends Component {
 
   render() {
     return(
-      <h1></h1>
+      <div>
+        <h1>City: </h1>
+        <h2>Current Condition: </h2>
+        <h2>Current Day Here </h2>
+        <h2>Current Temperature Here</h2>
+        <p>Expected High: </p>
+        <p>Expected Low: </p>
+        <p>Summary: "Windy, chance of late thunderstorms"</p>
+      </div>
     )
   }
 }
