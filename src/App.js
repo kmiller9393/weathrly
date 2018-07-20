bc919f1074f669e9b7ddc40d8b05108f809acd8a
Alex Bruce <brucealmightyalex@yahoo.com>
2018-07-20 16:16:59 -0600

    modify styling for all components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import CurrentWeather from './CurrentWeather.js';
+import SevenHour from './SevenHour.js';
 import Search from './Search.js';
 import Card from './Card.js';
 import { state } from './cleaners.js';
 import { city } from './cleaners.js';
 import KEY from './apikeys.js';
 import './App.css';
+import TenDay from './TenDay';
 
 
 class App extends Component {
@@ -43,15 +45,17 @@ class App extends Component {
         <section className="main-section">
           <Search />
           <CurrentWeather 
-          city={this.state.city}
-          condition={this.state.condition}
-          day={this.state.day}
-          temp={this.state.temp}
-          high={this.state.high}
-          low={this.state.low}
+            city={this.state.city}
+            condition={this.state.condition}
+            day={this.state.day}
+            temp={this.state.temp}
+            high={this.state.high}
+            low={this.state.low}
           />
+
+          <SevenHour />
           {/* <Card /> */}
-          <article className="app-component"></article>
+          <TenDay />
         </section>
       </div>
     );
