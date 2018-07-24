748ce5c12311dad544e4331b3a49f7396a80b395
Alex Bruce <brucealmightyalex@yahoo.com>
2018-07-24 12:59:07 -0600

    Add font to welcome page

--- src/Welcome.js
+++ src/Welcome.js
@@ -4,7 +4,7 @@ export default function Welcome() {
   return (
     <header className="App-header">
         {/* <img alt="weather logo" src={logo} className="App-logo" alt="logo" /> */}
-        <h1 className="App-title">Welcome to Weathrly</h1>
+        <h1 className="App-title">Weathrly</h1>
     </header>
   );
 }
