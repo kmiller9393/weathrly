1358c847990d93e0d27a42c4635aae03fcb59da2
Kurt Miller <kmiller9393@gmail.com>
2019-01-21 23:45:04 -0700

    Update index.css styling and wrap App in BrowserRouter component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
 require('dotenv').config();
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
 registerServiceWorker();
