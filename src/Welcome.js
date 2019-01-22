cde6da162b6bfa325fba4a5f76efa19a62f89a25
Kurt Miller <kmiller9393@gmail.com>
2019-01-21 23:44:40 -0700

    Move components into their own directories, clean up code

--- src/Welcome.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Welcome() {
-  return (
-    <header className="App-header">
-      <h1 className="App-title">Weathrly</h1>
-    </header>
-  );
-}
