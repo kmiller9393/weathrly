55ac1f3292c423d62c84b265444e087eb8bc0a99
Kurt Miller <kmiller9393@gmail.com>
2019-01-22 17:20:56 -0700

    Add getIcon functionality so better looking icons are now used, update styling

--- src/components/CurrentWeather/CurrentWeather.js
+++ src/components/CurrentWeather/CurrentWeather.js
@@ -1,38 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { getCurrentIcon } from '../../utils/getCurrentIcon';
 import './CurrentWeather.css';
 
-export default function CurrentWeather(props) {
-  const {
-    icon,
-    location,
-    currentDay,
-    conditions,
-    overview,
-    currentTemp,
-    currentHigh,
-    currentLow
-  } = props.currentInformation;
+export default class CurrentWeather extends Component {
+  getIcon = icon => {
+    if (icon) {
+      const weatherIcon = getCurrentIcon(icon);
 
-  return (
-    <div className="current-weather-container forecast-container">
-      <h1 className="current-weather-city">
-        <img alt="weather logo" className="weather-icon" src={icon} />
-        {location}
-      </h1>
-      <div className="day-conditions-container">
-        <h2 className="current-weather-day">{currentDay} </h2>
-        <h2 className="current-weather-condition">{conditions}</h2>
-        <p>{overview}</p>
-        <h2 className="current-weather-temp">{currentTemp}</h2>
+      return weatherIcon;
+    }
+  };
+
+  render() {
+    const {
+      icon,
+      location,
+      currentDay,
+      conditions,
+      overview,
+      currentTemp,
+      currentHigh,
+      currentLow
+    } = this.props.currentInformation;
+
+    return (
+      <div className="current-weather-container">
+        <h1 className="current-weather-city">
+          <img
+            alt="weather logo"
+            className="weather-icon"
+            src={this.getIcon(icon)}
+          />
+          {location}
+        </h1>
+        <div className="day-conditions-container">
+          <h2 className="current-weather-day">{currentDay} </h2>
+          <h2 className="current-weather-condition">{conditions}</h2>
+          <p>{overview}</p>
+          <h2 className="current-weather-temp">{currentTemp}</h2>
+        </div>
+        <article className="expected-temps">
+          <p className="current-weather-high">
+            High:<strong>{currentHigh}</strong>
+          </p>
+          <p className="current-weather-low">
+            Low:<strong>{currentLow}</strong>
+          </p>
+        </article>
       </div>
-      <article className="expected-temps">
-        <p className="current-weather-high">
-          High:<strong>{currentHigh}</strong>
-        </p>
-        <p className="current-weather-low">
-          Low:<strong>{currentLow}</strong>
-        </p>
-      </article>
-    </div>
-  );
+    );
+  }
 }
