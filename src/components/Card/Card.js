e4e1849b460e1120a1448e56c44b670980c43196
Kurt Miller <kmiller9393@gmail.com>
2019-01-22 17:20:24 -0700

    Update Card.js to use ES6 syntax

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,21 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default function Card(props) {
-  const { day, hour, temp, icon, high, low } = props;
+export default class Card extends Component {
+  render() {
+    const { day, hour, temp, icon, high, low } = this.props;
 
-  return (
-    <div className="card-template">
-      <p className="card-day">{day}</p>
-      <p className="card-hour">{hour}</p>
-      <p className="card-temp">{temp}</p>
-      <img src={icon} alt="weather icon" />
-      <article className="day-extremes-container">
-        <p className="day-extremes-high">
-          {high}
-          <strong> / </strong>
-        </p>
-        <p className="day-extremes-low">{low}</p>
-      </article>
-    </div>
-  );
+    return (
+      <div className="card-template">
+        <p className="card-day">{day}</p>
+        <p className="card-hour">{hour}</p>
+        <p className="card-temp">{temp}</p>
+        <img src={icon} alt="weather icon" />
+        <article className="day-extremes-container">
+          <p className="day-extremes-high">
+            {high}
+            <strong> / </strong>
+          </p>
+          <p className="day-extremes-low">{low}</p>
+        </article>
+      </div>
+    );
+  }
 }
