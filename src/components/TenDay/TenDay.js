85f46c4d80f8321195229500552a939c6bba5640
Kurt Miller <kmiller9393@gmail.com>
2019-01-22 17:22:22 -0700

    Update functionality in TenDay.js so better-looking icons are in use

--- src/components/TenDay/TenDay.js
+++ src/components/TenDay/TenDay.js
@@ -1,25 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Card from '../Card/Card.js';
+import { getTenDayIcons } from '../../utils/getTenDayIcons';
 import './TenDay.css';
 
-export default function TenDay(props) {
-  let tenDays = props.tenDays;
+export default class TenDay extends Component {
+  getIcon = icon => {
+    if (icon) {
+      const weatherIcon = getTenDayIcons(icon);
 
-  return (
-    <div className="forecast-container ten-day-container">
-      <h2 className="ten-day-header">Ten Day Forecast</h2>
-      <article className="ten-day-card-container">
-        {tenDays.map(day => {
-          return (
-            <Card
-              day={day.date.weekday}
-              icon={day.icon_url}
-              high={day.high.fahrenheit + '°F'}
-              low={day.low.fahrenheit + '°F'}
-            />
-          );
-        })}
-      </article>
-    </div>
-  );
+      return weatherIcon;
+    }
+  };
+
+  render() {
+    let tenDays = this.props.tenDays;
+
+    return (
+      <div className="ten-day-container">
+        <h2 className="ten-day-header">Ten Day Forecast</h2>
+        <article className="ten-day-card-container">
+          {tenDays.map(day => {
+            return (
+              <Card
+                day={day.date.weekday}
+                icon={this.getIcon(day.icon)}
+                high={day.high.fahrenheit + '°F'}
+                low={day.low.fahrenheit + '°F'}
+              />
+            );
+          })}
+        </article>
+      </div>
+    );
+  }
 }
